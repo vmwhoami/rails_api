e28666c83f975f86747fb99a73caea281a1283ff
Vitalie Melnic <vmwhoami@gmail.com>
2021-01-24 18:02:48 +0200

    generate rspec rails

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module RailsApi
     # the framework and any gems in your application.
 
     # Don't generate system test files.
+    config.api_only = true
     config.generators.system_tests = nil
   end
 end
