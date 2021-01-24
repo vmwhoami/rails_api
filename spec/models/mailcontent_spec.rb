5e28b73f46dcebcf2a5b73703771818b8d320a48
Vitalie Melnic <vmwhoami@gmail.com>
2021-01-24 18:55:22 +0200

    write tests for mailcontent model

--- spec/models/mailcontent_spec.rb
+++ spec/models/mailcontent_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Mailcontent, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should validate of the model mailcontent' do
+    expect(FactoryBot.build :mailcontent).to be_valid  
+  end
+
+  it 'should validate the precense of email' do
+    vitalie = FactoryBot.build :mailcontent, :name => ''
+    expect(vitalie).not_to be_valid
+    expect(vitalie.error.messages[:name]).to include('Name has to be present')
+  end
+ 
+  it 'should validate the precense of the phone name' do
+    
+  end
 end
