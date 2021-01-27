645887551c6b3ad88ca63d4193a287a16ba8e5b2
vmwhoami <vmwhoami@gmail.com>
2021-01-27 17:24:11 +0200

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  get '/mailcontent', to: 'mailcontent#index'
-  get '/mailcontent/:id', to: 'mailcontent#show'
-  # resources :mailcontent ,only:  [:index]
+  # get '/mailcontent', to: 'mailcontent#index'
+  # get '/mailcontent/:id', to: 'mailcontent#show'
+  resources :mailcontent ,only:  [:index, :show]
 end
