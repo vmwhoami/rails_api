c91c18324ee24c71a3a7b74ba4e6267db6da3241
vmwhoami <vmwhoami@gmail.com>
2021-01-27 16:47:26 +0200

    create seed file with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,11 @@ lastname = ["Melnic","Vidrascu","Coleadov","Pirvu","Bogda"]
 
 10.times do |u|
   Mailcontent.create!(
-    name: "User#{@lastname.sample}",
-    email: "User Family_#{@lastname.sample}",
-    coverimage: @cover_image.sample,
-    photo: @photos.sample
+    name: " #{firstname.sample} #{lastname.sample}",
+    email: "vmwhoami#{u}@gmail.com",
+    phone: "+3736944247#{u}",
+    content:" Lorem ipsum, dolor sit amet consectetur adipisicingu elit. Explicabo nulla voluptates obcaecati a minus ratione? Repudiandae provident voluptatem alias nisi."
   )
 end
+
+puts "---"*100
