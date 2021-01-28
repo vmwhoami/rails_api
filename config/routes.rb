6a1609aa68f58786ecd4d2ea10df8e17dca79e9e
vmwhoami <vmwhoami@gmail.com>
2021-01-28 16:51:13 +0200

    change the root route to be mailcontent#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  
-  get "/mailcontent", to: "mailcontent#index"
+ 
+  get "/", to: "mailcontent#index"
+  get "/:id",to: "mailcontent#show"
   # resources :mailcontent, only: [:index,:show]
 end
